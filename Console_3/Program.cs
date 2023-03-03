15fbf17ea78e22898e0c4c0408dd4ef5422a2089
Yasha <yashasidin333@yandex.ru>
2023-03-03 11:54:43 +0300

    Add Console_3

---
+++ Console_3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Вводим четверть, выводим диапозон возможных значений
+void Quarter(int x)
+{
+    if (x == 1)
+    {
+        Console.WriteLine("x > 0; y > 0");
+    }
+    if (x == 2)
+    {
+        Console.WriteLine("x < 0; y > 0");
+    }
+    if (x == 3)
+    {
+        Console.WriteLine("x < 0; y < 0");
+    }
+    if (x == 4)
+    {
+        Console.WriteLine("x > 0; y < 0");
+    }
+}
+Console.Write("Введите номер четверти: ");
+int Q = int.Parse(Console.ReadLine());
+
+Quarter(Q);
