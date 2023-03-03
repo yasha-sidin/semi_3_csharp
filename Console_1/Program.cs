1efb696a18099e92a3fbd42766e90a04ef56bd0a
Yasha <yashasidin333@yandex.ru>
2023-03-03 11:12:15 +0300

    Add Console_1

---
+++ Console_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// While
+int count = 0;
+while (count < 100)
+{
+    Console.WriteLine($"{count + 1}. Hello world");
+    count++;
+}
+Console.WriteLine("Выход из цикла while");
+
+// for
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine($"{i + 1}. Hello world");
+}
+Console.WriteLine("Выход из цикла for");
