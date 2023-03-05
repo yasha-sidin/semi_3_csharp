3c21a1acad5c92361a4422391af56f673d128841
Yasha <yashasidin333@yandex.ru>
2023-03-05 14:31:38 +0300

    Add Console_4

---
+++ Console_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Принимаем координаты двух точек; находим расстояние между ними
+double Distance(double xa, double ya, double xb, double yb)
+{
+    double result = Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2));
+    return result;
+}
+Console.Write("Введите коордитнаты первой точки через Enter: ");
+double xa = double.Parse(Console.ReadLine());
+double ya = double.Parse(Console.ReadLine());
+Console.Write("Введите коордитнаты второй точки через Enter: ");
+double xb = double.Parse(Console.ReadLine());
+double yb = double.Parse(Console.ReadLine());
+
+double distance = Distance(xa, ya, xb, yb);
+Console.WriteLine($"Расстояние между двумя точками равно: {Math.Round(distance, 2, MidpointRounding.ToNegativeInfinity)}");
