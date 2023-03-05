8ea72843b4450e405b200af470d41b474292ba19
Yasha <yashasidin333@yandex.ru>
2023-03-05 14:41:25 +0300

    Add Console_5

---
+++ Console_5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу квадратов чисел от 1 до N
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 1; i < number; i++)
+{
+    Console.Write($"{Math.Pow(i, 2)}, ");
+}
+Console.Write($"{Math.Pow(number, 2)}");
